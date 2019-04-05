5432564f47fcfa14412deed17ba281a28f0d71f7
sonamkumarijnv <sonam@winds.co.in>
2019-04-05 07:53:00 +0530

    initial commit

---
+++ ES6_boilerplate/app/index.js
@@ -0,0 +1,16 @@
+//Template Literals
+console.log("time to starte fresh")
+let a=`hello`;
+let b=`${a} sonam`;
+console.log(b)
+//Spread  opertator is new way to operat or manuplate  array and object
+// let a = [20, 30, 40];
+// let b = [10,   ...a, 50];
+// console.log(b)
+//object Destructuring
+// let z = [4,5,6];
+// let four = z[0]
+// let five = z[1]
+// console.log(four,five)
+// let [four,five]=z;
+// console.log(four,five)
